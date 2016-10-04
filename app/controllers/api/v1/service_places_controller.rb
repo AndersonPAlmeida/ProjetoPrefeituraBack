a65f166954e46cb2cf8a2eb6cb264803878f8f2a
Bruno Freitas Tissei <bft15@inf.ufpr.br>
2016-10-04 11:25:39 -0300

    Fixed service_places_controller (lacking tests)
    Signed-off-by: Bruno Freitas Tissei <bft15@inf.ufpr.br>

--- app/controllers/api/v1/service_places_controller.rb
+++ app/controllers/api/v1/service_places_controller.rb
@@ -11,7 +11,13 @@ module Api::V1
 
 	  # GET /service_places/1
 	  def show
-	    render json: @service_place
+      if @service_place.nil?
+        render json: {
+          errors: ["Service place #{params[:id]} does not exist."]
+        }, status: 404
+      else
+	      render json: @service_place
+      end
 	  end
 
 	  # POST /service_places
@@ -27,28 +33,57 @@ module Api::V1
 
 	  # PATCH/PUT /service_places/1
 	  def update
-	    if @service_place.update(service_place_params)
-	      render json: @service_place
-	    else
-	      render json: @service_place.errors, status: :unprocessable_entity
-	    end
+      if @service_place.nil?
+        render json: {
+          errors: ["Service place #{params[:id]} does not exist."]
+        }, status: 404
+      else
+        if @service_place.update(service_place_params)
+          render json: @service_place
+        else
+          render json: @service_place.errors, status: :unprocessable_entity
+        end
+      end
 	  end
 
 	  # DELETE /service_places/1
 	  def destroy
-	    @service_place.destroy
+      if @service_place.nil?
+        render json: {
+          errors: ["Service place #{params[:id]} does not exist."]
+        }, status: 404
+      else
+  	    @service_place.active = false
+        @service_place.save
+      end
 	  end
 
-	  private
-	    # Use callbacks to share common setup or constraints between actions.
-	    def set_service_place
+	private
+
+	  # Use callbacks to share common setup or constraints between actions.
+	  def set_service_place
+      begin
 	      @service_place = ServicePlace.find(params[:id])
-	    end
+      rescue
+        @service_place = nil
+      end
+	  end
 
-	    # Only allow a trusted parameter "white list" through.
-	    def service_place_params 
-	      params.require(:service_place).permit(:active, :address_number, :address_street, :name, :neighborhood, :address_complement, :cep, :email, :phone1, :phone2, :url)
-	    end
-
+	  # Only allow a trusted parameter "white list" through.
+	  def service_place_params 
+	    params.require(:service_place).permit(
+        :active, 
+        :address_number, 
+        :address_street, 
+        :name, 
+        :neighborhood, 
+        :address_complement, 
+        :cep, 
+        :email, 
+        :phone1, 
+        :phone2, 
+        :url
+      )
+	  end
 	end
 end
