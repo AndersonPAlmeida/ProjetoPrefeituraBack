55cf04a8fbc875fe279dc1382274a3256a285b93
Bruno Freitas Tissei <bft15@inf.ufpr.br>
2017-10-06 10:16:27 -0300

    Add address_street and address_number to schedule history request
    Signed-off-by: Bruno Freitas Tissei <bft15@inf.ufpr.br>

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -40,6 +40,8 @@ class Schedule < ApplicationRecord
       sector_name: self.shift.service_type.sector.name,
       service_type_name: self.shift.service_type.description,
       service_place_name: self.service_place.name,
+      service_place_address_street: self.service_place.address_street,
+      service_place_address_number: self.service_place.address_number,
       situation: self.situation.description
     })
   end
@@ -87,6 +89,55 @@ class Schedule < ApplicationRecord
   # @return [ActiveRecords] filtered schedules
   def self.filter(params, npage)
     return search(search_params(params), npage)
+  end
+
+  def self.history_form(citizen)
+    # =========================== Sectors ===========================
+    sectors = Sector.all_active.local(citizen.city_id)
+      .as_json(only: [:name, :id])
+
+    sector_ids = sectors.map { |row| row["id"] }
+
+
+    # ======================== Service Types ========================
+    service_types = ServiceType.where(sector_id: sector_ids, active: true)
+      .as_json(only: [:description, :id, :sector_id])
+
+    service_type_ids = service_types.map { |row| row["id"] }
+
+
+    # ======================= Service Places ========================
+    service_types = ServiceType.where(id: service_type_ids)
+    ids = service_types.map { |i| i.service_place_ids }.flatten.uniq!
+
+    st_ids = Hash.new
+
+    service_places = ServicePlace.where(id: ids, active: true)
+    service_places_resp = service_places.as_json(only: [:name, :id])
+
+    for i in service_places
+      st_ids[i.id.to_s] = i.service_type_ids
+    end
+
+    for i in service_places_resp 
+      i["service_types"] = st_ids[i["id"].to_s]
+    end
+
+    service_places_resp.as_json
+
+
+    # ========================== Situations =========================
+    situations = Situation.all.as_json(only: [:id, :description])
+
+
+    # ========================== Form Data ==========================
+    response = Hash.new
+    response[:sectors]       = sectors
+    response[:service_type]  = service_types 
+    response[:service_place] = service_places
+    response[:situation]     = situations
+
+    return response.as_json
   end
 
   private
