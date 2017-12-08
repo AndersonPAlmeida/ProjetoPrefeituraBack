b1423a368a3a326abbc3f6a25295c6edbfd765cb
Bruno Freitas Tissei <bft15@inf.ufpr.br>
2017-12-08 10:57:33 -0200

    Add sector list to schedule history request
    Signed-off-by: Bruno Freitas Tissei <bft15@inf.ufpr.br>

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -56,33 +56,43 @@ class Schedule < ApplicationRecord
   # @params params [ActionController::Parameters] Parameters for searching
   # @params npage [String] number of page to be returned
   # @return [Json] every schedule for each dependant from a citizen and the 
-  #   citizen himself for showing in the schedule history screen
+  # citizen himself for showing in the schedule history screen
   def self.citizen_history(id, params, npage)
-
     # Citizen's dependants
     citizens = Citizen.where(responsible_id: id).pluck(:id, :name)
 
     response = Hash.new.as_json
     
+    schedules = Schedule.where(citizen_id: id)
+    sectors = get_sectors_response(schedules.where(situation_id: 2))
+    
+    schedules = schedules.filter(params, npage)
+      .map { |i| i.show_data }
+
     # Citizen's info along with the schedules associated with him
     response["id"] = id
     response["name"] = Citizen.find(id).name
-    response["schedules"] = Schedule.where(citizen_id: id)
-      .filter(params, npage)
-      .map { |i| i.show_data }.as_json
-      
-
+    response["schedules"] = Hash.new.as_json
+    response["schedules"]["entries"] = schedules.as_json
+    response["schedules"]["sectors"] = sectors
     response["dependants"] = [].as_json
+
 
     # Schedules assigned to each citizen's dependant
     citizens.each do |i,name|
+      schedules = Schedule.where(citizen_id: i)
+      sectors = get_sectors_response(schedules.where(situation_id: 2))
+      schedules = schedules.filter(params, npage)
+        .map { |j| j.show_data }
+
+
       entry = Hash.new.as_json
 
       entry["id"] = i
       entry["name"] = name
-      entry["schedules"] = Schedule.where(citizen_id: i)
-        .filter(params, npage)
-        .map { |i| i.show_data }.as_json
+      entry["schedules"] = Hash.new.as_json
+      entry["schedules"]["entries"] = schedules.as_json
+      entry["schedules"]["sectors"] = sectors
 
       response["dependants"].append(entry)
     end
@@ -98,6 +108,26 @@ class Schedule < ApplicationRecord
   end
 
   private
+
+  def self.get_sectors_response(schedules)
+    # Get every sector_id present in schedules
+    sector_ids = schedules.joins({shift: :service_type})
+      .pluck(:"service_types.sector_id")
+
+    # Count frequency of each sector
+    freq = sector_ids.each_with_object(Hash.new(0)) { |key,hash| 
+      hash[key] += 1
+    }
+
+    # Build array of sectors containing the amount of schedules
+    # of the given citizen in each present sector
+    sectors = Sector.where(id: sector_ids)
+      .as_json(only: [:id, :name, :schedules_by_sector])
+      .each_with_object([]) { |key, hash| 
+        key["schedules"] = freq[key["id"]]; 
+        hash.append(key)
+      }
+  end
 
   # Translates incoming search parameters to ransack patterns
   # @params params [ActionController::Parameters] Parameters for searching
