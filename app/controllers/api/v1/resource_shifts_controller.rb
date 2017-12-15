4af0c61e7620a2461c0a81be921ecac487a61888
Henrique V. Ehrenfried <hve13@inf.ufpr.br>
2017-12-15 08:26:14 -0200

    Fix bug when get professional name and fix setup_exemplos
    Signed-off-by: Henrique V. Ehrenfried <hve13@inf.ufpr.br>

--- app/controllers/api/v1/resource_shifts_controller.rb
+++ app/controllers/api/v1/resource_shifts_controller.rb
@@ -10,7 +10,7 @@ module Api::V1
         professional_name = Citizen.where(
                             account_id:Account.where(
                                   id:Professional.where(
-                                      id: ProfessionalsServicePlace.where(id: params[:id]).first.professional_id
+                                      id: params[:id]
                                   ).first.id
                             ).first.id 
                           ).first.name
