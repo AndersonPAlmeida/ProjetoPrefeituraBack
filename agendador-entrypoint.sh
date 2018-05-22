b91622e473f41c85900e9b246d9c16cb1d055f8d
Matheus Horstmann <mch15@inf.ufpr.br>
2018-05-22 10:18:01 -0300

    Add db:migrate in case of production

--- agendador-entrypoint.sh
+++ agendador-entrypoint.sh
@@ -6,6 +6,10 @@ set -e
 if [ "$1" = 'CREATE' ]; then
 /app/bin/rake agendador:setup
 fi
+
+if [ "$1" = 'PRODUCTION' ]; then
+/app/bin/rake db:migrate
+fi
 /app/bin/bundle exec sidekiq -C config/sidekiq.yml >> log/sidekiq.log &
 /app/bin/bundle exec rails s -p 3000 -b '0.0.0.0'
 
