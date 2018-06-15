59bd2daa0195d64067f9e0b4a1cee9694cd0cb85
Rafael Ravedutti <rrlmachado@inf.ufpr.br>
2018-06-15 11:06:55 +0000

    Adjust get citizen uploads format
    Signed-off-by: Rafael Ravedutti <rrlmachado@inf.ufpr.br>

--- app/models/citizen_upload.rb
+++ app/models/citizen_upload.rb
@@ -1,4 +1,6 @@
 class CitizenUpload < ApplicationRecord
+  include Searchable
+
   belongs_to :citizen
 
   # Specify location where the log should be stored (default is public)
@@ -8,5 +10,24 @@ class CitizenUpload < ApplicationRecord
   # Validates format of logs
   validates_attachment_content_type :log,
     :content_type => ["text/plain", "text/csv"]
+
+  # @params params [ActionController::Parameters] Parameters for searching
+  # @params npage [String] number of page to be returned
+  # @params permission [String] Permission of current user
+  # @return [ActiveRecords] filtered citizens 
+  def self.filter(params, npage, permission)
+    return search(search_params(params, permission), npage)
+  end
+
+  # Translates incoming search parameters to ransack patterns
+  # @params params [ActionController::Parameters] Parameters for searching
+  # @params permission [String] Permission of current user
+  # @return [Hash] filtered and translated parameters
+  def self.search_params(params, permission)
+    sortable = ["created_at"]
+    filter = {"citizen_id" => "citizen_id_eq", "created_at" => "created_at_gteq"}
+
+    return filter_search_params(params, filter, sortable)
+  end
 
 end
